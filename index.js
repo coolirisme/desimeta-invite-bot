2b5b771f1c24d0b53517002a6f2efa8f005da4a8
coolirisme <coolirisme@gmail.com>
2020-12-06 13:54:38 +0530

    var -> let

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ let db = new Sqlite3.Database('./users.db', (err) => {
 //Utility function for UUID generation
 const uuidv4 = () => {
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-    var r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
+    let r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
     return v.toString(16);
   });
 }
